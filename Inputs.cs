8998210d503f92461a601f475bd131b2543fecce
Julian <julianres28@hotmail.com>
2023-10-28 15:46:29 +0200

    Takes move as input and checks that its the correct format

---
+++ Inputs.cs
@@ -0,0 +1,46 @@
+using System.Text.RegularExpressions;
+
+public class Inputs {
+    
+    public static (int[], int[]) ParseInput(string input) {
+
+        // if(input is null || !InputValid(input)) {
+        //     return null;
+        // }
+
+        int[] origin = new int[2];
+        int[] destination = new int[2];
+
+        string regexPatter = @"\d";
+        
+        MatchCollection m = Regex.Matches(input, regexPatter, RegexOptions.Compiled);
+
+        int i = 0;
+        foreach(Match match in m) {
+            if(i < 2) {
+                origin[i%2] = Convert.ToInt32(match.Value);
+            }
+            else {
+                destination[i%2] = Convert.ToInt32(match.Value);
+            }
+            i++;
+        }
+
+        return (origin, destination);
+    }
+
+    public static bool InputValid(string? input) {
+        if(input is null) {
+            return false;
+        }
+        string regexPattern = @"^[1-8],[1-8] [1-8],[1-8]$";
+
+        Match m = Regex.Match(input, regexPattern, RegexOptions.Compiled);
+        
+        if(!m.Success) {
+            Console.WriteLine("Invalid Input");
+        }
+
+        return m.Success;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,3 +2,21 @@
 
 Board theBoard = new Board();
 theBoard.FillBoard();
+
+while(true) {
+    Console.WriteLine("Give me something: int,int int,int");
+    string? moveInput = Console.ReadLine();
+
+    Console.WriteLine($"You said: {moveInput}");
+
+    if(moveInput is null || !Inputs.InputValid(moveInput)) {
+        Console.WriteLine("Please try again with the propper format");
+        continue;
+    }
+
+    // var move = Inputs.ParseInput(moveInput);
+    (int[] origin, int[] destination) = Inputs.ParseInput(moveInput);
+
+    Console.WriteLine($"Move {origin[0]},{origin[1]} {destination[0]},{destination[1]}");
+    
+}
